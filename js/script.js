fdf6edc44721c5738d14f750d050b50c0528e756
Vitória Marquione <111090887+vitoriamarquione@users.noreply.github.com>
2025-04-07 10:08:15 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import initFilterTab from "./modules/filtertab.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initColorScheme from "./modules/color-scheme.js";
+
+initFilterTab();
+initMenuMobile();
+initColorScheme();
